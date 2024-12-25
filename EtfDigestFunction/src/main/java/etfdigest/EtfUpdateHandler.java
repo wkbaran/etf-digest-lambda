7ba23b67d4cd1db3350f8f5b868132743c76f4e7
Bill Baran <bill.baran@gmail.com>
2024-12-24 18:09:32 -0700

    Migrating off hello world

--- EtfDigestFunction/src/main/java/etfdigest/EtfUpdateHandler.java
+++ EtfDigestFunction/src/main/java/etfdigest/EtfUpdateHandler.java
@@ -1,4 +1,4 @@
-package helloworld;
+package etfdigest;
 
 import java.io.IOException;
 import java.io.InputStream;
--- EtfDigestFunction/src/test/java/etfdigest/EtfUpdateHandlerTest.java
+++ EtfDigestFunction/src/test/java/etfdigest/EtfUpdateHandlerTest.java
@@ -1,4 +1,4 @@
-package helloworld;
+package etfdigest;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
